288952acf7b24a18c4b73959f5b64cd89a674cae
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2015-12-07 19:04:20 +0500

    First commit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,18 +8,73 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber = false
+    var nextOperand:String = ""
+    var operandArray = Array<Double>()
+
+    @IBAction func appendDigit(sender: UIButton)
+    {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber{
+            display.text = display.text! + digit
+        }else{
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTypingANumber {
+            storeDisplayValue()
+        }
+        if operandArray.count >= 2 || operation == "="{
+            switch nextOperand{
+                case "+": performOperation { $0 + $1 }
+                case "−": performOperation { $0 - $1 }
+                case "×": performOperation { $0 * $1 }
+                case "÷": performOperation { $1 / $0 }
+                default: break
+            }
+        }
+        nextOperand = operation
+       
     }
-
-
+    
+    @IBAction func clearAll() {
+        operandArray.removeAll()
+        userIsInTheMiddleOfTypingANumber = false
+        displayValue = 0
+    }
+    func performOperation ( operation: (Double, Double)-> Double){
+        var result: Double = 0.0
+        if operandArray.count >= 2 {
+            result =  operation( operandArray.removeLast(), operandArray.removeLast())
+            displayValue = result
+            operandArray.append(result)
+        }
+    }
+    func storeDisplayValue () {
+        
+        userIsInTheMiddleOfTypingANumber = false
+        operandArray.append(displayValue)
+    }
+    
+    var displayValue:Double {
+        get{
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set{
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
+    
 }
 
